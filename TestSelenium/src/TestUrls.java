9b7009abede03badf8f4455ba2a0e28cdf697ef1
DCTC-Vinay <94598337+DCTC-Vinay@users.noreply.github.com>
2021-11-18 13:39:08 +0530

    Core Java

--- TestSelenium/src/TestUrls.java
+++
@@ -1,7 +0,0 @@
-
-public class TestUrls {
-
-	
-	System.setProperty("webdriver.gecko.driver","C:\\Users\\ip-slim-3\\Desktop\\Selenium Browser Drivers\\geckodriver-v0.30.0-win64\\geckodriver.exe");
-	
-}
