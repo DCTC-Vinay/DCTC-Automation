5ea37fff5763490f7fae1a83c2ff310e26b0876a
DCTC-Vinay <94598337+DCTC-Vinay@users.noreply.github.com>
2022-02-19 07:47:35 +0530

    Updated on 19.02

---
+++ Dec 2021_Core Java/arrayinjava/ArrayInJava.java
@@ -0,0 +1,18 @@
+package arrayinjava;
+
+public class ArrayInJava {
+	
+	public static void main(String[] args) {
+		
+		int a[]=new int[2];
+		
+		a[0]=23;
+		a[1]=26;
+		
+		System.out.println(a[1]);
+		
+		
+		
+	}
+
+}
--- Dec 2021_Core Java/switchinjava/SwitchInJava.java
+++ Dec 2021_Core Java/switchinjava/SwitchInJava.java
@@ -6,8 +6,43 @@ public class SwitchInJava {
 		// TODO Auto-generated method stub
 		
 		SwitchInJava s=new SwitchInJava();
+		s.takeInputChar1('b', 'a');
 		s.takeInputChar('b',3);
+		
 		//s.takeInput(1);
+	}
+	
+	public void takeInputChar1(char c,char d)
+	{
+		switch(c)
+		{
+			case 'A':
+			case 'a':
+				System.out.println("In case a");
+				break;
+			case 'b':
+				System.out.println("In Case b");
+				
+				switch(d)
+				{
+				case 'a':
+					System.out.println("In Case 1");
+					break;
+				case 'b':
+					System.out.println("In Case 2");
+					break;
+				default:
+					System.out.println("Default Case");
+					break;
+				}
+				break;
+			case '1':
+				System.out.println("In Case 1");
+				break;
+			default:
+				System.out.println("No option Found");
+				break;
+		}
 	}
 	
 	public void takeInputChar(char c,int d)
