a5249a7693f684000a146fa38c3b8235b85460b4
DCTC-Vinay <94598337+DCTC-Vinay@users.noreply.github.com>
2022-02-26 23:54:59 +0530

    Update 26.02

---
+++ Dec 2021_Core Java/polymorphism/A.java
@@ -0,0 +1,10 @@
+package polymorphism;
+
+public class A {
+	
+	public void show()
+	{
+		System.out.println("Inside show method of A Class");
+	}
+
+}
---
+++ Dec 2021_Core Java/polymorphism/B.java
@@ -0,0 +1,9 @@
+package polymorphism;
+
+public class B extends A {
+	
+	public void show()
+	{
+		System.out.println("Inside show method of B Class");
+	}
+}
