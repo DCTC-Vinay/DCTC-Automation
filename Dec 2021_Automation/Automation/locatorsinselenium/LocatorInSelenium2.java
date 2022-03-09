d7117ebfe4095cbc6d98d78b82d35c34f1826dec
DCTC-Vinay <94598337+DCTC-Vinay@users.noreply.github.com>
2022-03-09 22:05:56 +0530

    update on 09.03

--- Dec 2021_Automation/Automation/locatorsinselenium/LocatorInSelenium2.java
+++ Dec 2021_Automation/Automation/locatorsinselenium/LocatorInSelenium2.java
@@ -5,31 +5,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class LocatorInSelenium {
+public class LocatorInSelenium2 {
 
+	WebDriver driver;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		LocatorInSelenium2 lis=new LocatorInSelenium2();
+		lis.pretest();
+	}
+	public void pretest()
+	{
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Selenium Browser Drivers\\Chrome\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
-		
+		login_page(driver);
+	}
+	
+	public void login_page(WebDriver driver)
+	{
+		this.driver=driver;
 		WebElement email = driver.findElement(By.id("email"));
 		email.sendKeys("test@gmail.com");
-		
-	//	driver.findElement(By.id("pass")).sendKeys("12345");
-		
-		WebElement password = driver.findElement(By.id("pass"));
-		password.sendKeys("1234576");
-		
-	//	driver.findElement(By.name("login")).click();
-		
-	//	driver.findElement(By.tagName("a")).click();
-		
-		driver.findElement(By.linkText("Forgotten password?")).click();
-		
-		driver.findElement(By.partialLinkText("Voting")).click();
 	}
-
 }
