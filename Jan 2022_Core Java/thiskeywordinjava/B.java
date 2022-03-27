af96a919c6a52592ba7b39cd94aef9934f743252
DCTC-Vinay <94598337+DCTC-Vinay@users.noreply.github.com>
2022-03-27 08:12:21 +0530

    update on 27.03

---
+++ Jan 2022_Core Java/thiskeywordinjava/B.java
@@ -0,0 +1,37 @@
+package thiskeywordinjava;
+
+public class B {
+
+	int id;
+	String empName;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		B b1=new B();
+		B b2=new B(123,"Vinay");
+		
+		b1.display();
+		b2.display();
+		
+	}
+	
+	public B()
+	{
+		empName="Test";
+		id=12122;
+		//this.display();
+	}
+	
+	public B(int id, String empName)
+	{
+		this.id=id;
+		this.empName=empName;
+		//this.display();
+	}
+
+	public void display()
+	{
+		System.out.println("Emp Id: "+id);
+		System.out.println("EmpName :"+empName);
+	}
+}
