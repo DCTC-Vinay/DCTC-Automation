d571c17c90226194c988578238c2beab3b7a6d17
DCTC-Vinay <94598337+DCTC-Vinay@users.noreply.github.com>
2022-08-17 21:51:11 +0530

    Update on 17.08

--- April 2022 Automation/alertsinselenium/AlertsInSelenium.java
+++ April 2022 Automation/alertsinselenium/AlertsInSelenium.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class AlertsInSelenium {
@@ -31,16 +33,48 @@ public class AlertsInSelenium {
 	
 	public void testAlert() throws InterruptedException
 	{
+		
+		//alert 1
 		driver.findElement(By.id("alertButton")).click();
 		
 		Alert a1=driver.switchTo().alert();
-		Thread.sleep(3000);
+		Thread.sleep(5000);
+		
 		System.out.println(a1.getText());
 		a1.accept();
+		
+		//alert 2 Timer alert
+		driver.findElement(By.id("timerAlertButton")).click();
+		
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.alertIsPresent());
+		
+		a1=driver.switchTo().alert();
+		
+		Thread.sleep(5000);
+		System.out.println(a1.getText());
+		a1.accept();
+		
+		//alert 3 Ok Cancel Button
+		driver.findElement(By.id("confirmButton")).click();
+		Thread.sleep(5000);
+		
+		a1=driver.switchTo().alert();
+		a1.dismiss();
+			
+		WebElement result=driver.findElement(By.id("confirmResult"));
+		System.out.println(result.getText());
+		
+		//alert 4 Sending Text in TextField of Alert
+		
+		driver.findElement(By.id("promtButton")).click();
+		Thread.sleep(5000);
+		
+		a1=driver.switchTo().alert();
+		a1.sendKeys("Vinay Salunke");
+		a1.accept();
+		
+		WebElement result1=driver.findElement(By.id("promptResult"));
+		System.out.println(result1.getText());
 	}
 }
-
-
-
-
-
